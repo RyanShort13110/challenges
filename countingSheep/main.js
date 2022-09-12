ab34b99e4d8556eb97e951bb465ec89d81adacbb
Ryan Short <ryanshort.webdev@gmail.com>
2022-09-12 15:57:39 -0400

    new challenge added

--- countingSheep/main.js
+++ countingSheep/main.js
@@ -1,5 +1,6 @@
 // Given a non-negative integer, 3 for example, return a string with a murmur: "1 sheep...2 sheep...3 sheep...". Input will always be valid, i.e. no negative integers.
 
+
 const countSheep = num => {
   let str = '';
   for (let i = 1; i <= num; i++) {
@@ -9,8 +10,8 @@ const countSheep = num => {
 }
 
 
-
 // test cases
+
 
 const chai = require("chai");
 const assert = chai.assert;
