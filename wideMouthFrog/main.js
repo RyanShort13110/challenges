802a5c41b4e37c18057a904fad3dfe7469f73e28
Ryan Short <ryanshort.webdev@gmail.com>
2022-10-24 22:02:05 -0400

    added 'wide mouth frog' challenge

--- wideMouthFrog/main.js
+++ wideMouthFrog/main.js
@@ -15,21 +15,3 @@ function mouthSize(animal) {
     return 'wide';
   }
 }
-
-
-
-
-// test cases
-
-
-const chai = require("chai");
-const assert = chai.assert;
-chai.config.truncateThreshold=0;
-
-describe("The Wide-Mouthed frog! ", () => {
-  it("Testing for fixed tests", () => {
-    assert.strictEqual(mouthSize("toucan"),"wide")
-    assert.strictEqual(mouthSize("ant bear"),"wide")
-    assert.strictEqual(mouthSize("alligator"),"small")
-  })
-})
