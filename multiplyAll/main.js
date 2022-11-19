5c5e122a4220c1091b6084f8ee27dd594b8e8588
Ryan Short <ryanshort.webdev@gmail.com>
2022-11-19 10:25:36 -0500

    added 'multiply all together' challenge

---
+++ multiplyAll/main.js
@@ -0,0 +1,9 @@
+// Your task is to create a function that takes in an array of numbers. 
+// Multiply each number together and alert the product. 
+
+
+function multiplyAll(arr){
+  let product = 1
+  arr.forEach(num => num *= product)
+  alert(product)
+}
