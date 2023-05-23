188b697a9b0cfcab1a110cd8edab1f5accccbdb2
Ryan Short <ryanshort.dev@gmail.com>
2023-05-23 16:21:04 -0400

    added 'camelCase' php challenge

---
+++ camelCase/camelCase.php
@@ -0,0 +1,24 @@
+<!-- Complete the method/function so that it converts dash/underscore delimited words into camel casing. The first word within the output should be capitalized only if the original word was capitalized (known as Upper Camel Case, also often referred to as Pascal case). The next words should be always capitalized.
+Examples
+"the-stealth-warrior" gets converted to "theStealthWarrior"
+
+"The_Stealth_Warrior" gets converted to "TheStealthWarrior" -->
+
+
+<!-- solution -->
+
+<?php 
+
+function reverseWords($str){
+  $str_arr = explode("-", $str);
+  foreach($str_arr as $i => &$word){
+    if($i === 0) {
+      $word = strtolower($word);
+    } else {
+      $word = ucfirst($word);
+    }
+  }
+  echo implode("", $str_arr);
+}
+
+// reverseWords('These are some more words');
